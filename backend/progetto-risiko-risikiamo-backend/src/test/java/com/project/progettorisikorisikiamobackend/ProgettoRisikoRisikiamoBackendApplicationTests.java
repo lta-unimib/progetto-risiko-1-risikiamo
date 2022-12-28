6ead1c3a6692d1e0bbe4ee45054d0db1bc20c93a
PmP-source <85564799+PmP-source@users.noreply.github.com>
2022-12-28 17:48:36 +0100

    frist issue dice

--- backend/progetto-risiko-risikiamo-backend/src/test/java/com/project/progettorisikorisikiamobackend/ProgettoRisikoRisikiamoBackendApplicationTests.java
+++ backend/progetto-risiko-risikiamo-backend/src/test/java/com/project/progettorisikorisikiamobackend/ProgettoRisikoRisikiamoBackendApplicationTests.java
@@ -11,5 +11,19 @@ class ProgettoRisikoRisikiamoBackendApplicationTests {
 		// Test per conferma coverege
 		assert true;
 	}
+	@Test // Test dado
+	void testDice() {
+		// Test per conferma coverege
+		Dice d = new Dice(6);
+		assert d.getSides() == 6;
+		assert d.getValue() == 0;
+		d.roll();
+		assert d.getValue() > 0;
+		assert d.getValue() <= 6;
+		d.setValue(5);
+		assert d.getValue() == 5;
+		d.setSides(10);
+		assert d.getSides() == 10;
+	}
 
 }
