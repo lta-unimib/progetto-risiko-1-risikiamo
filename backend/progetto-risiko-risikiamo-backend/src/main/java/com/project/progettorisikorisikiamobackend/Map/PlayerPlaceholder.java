bda68e75a44921364fdc1790f5deb1e036f1400e
MauroZorzin <m.zorzin@campus.unimib.it>
2022-12-29 16:49:32 +0100

    Continent

---
+++ backend/progetto-risiko-risikiamo-backend/src/main/java/com/project/progettorisikorisikiamobackend/Map/PlayerPlaceholder.java
@@ -0,0 +1,15 @@
+package com.project.progettorisikorisikiamobackend.Map;
+
+public class PlayerPlaceholder {
+
+    private String name;
+
+    public PlayerPlaceholder(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ backend/progetto-risiko-risikiamo-backend/src/test/java/com/project/progettorisikorisikiamobackend/Map/ContinetTests.java
@@ -0,0 +1,91 @@
+package com.project.progettorisikorisikiamobackend.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ContinetTests {
+
+    // Test: testContinet
+    @Test
+    public void testContinet() {
+
+        Continet continet = new Continet("Continet1", 1);
+        assertEquals("Continet1", continet.getName());
+        assertEquals(1, continet.getBonus());
+        assertEquals(0, continet.getTerritories().values().size());
+
+        Territory territory1 = new Territory("Territory1");
+        Territory territory2 = new Territory("Territory2");
+
+        continet.addTerritory(territory1, "id1");
+        continet.addTerritory(territory2, "id2");
+
+        assertEquals(2, continet.getTerritories().values().size());
+        assertEquals(territory1, continet.getTerritories().get("id1"));
+        assertEquals(territory2, continet.getTerritories().get("id2"));
+
+        assertThrows(IllegalArgumentException.class, () -> continet.addTerritory(territory1, "id1"));
+        assertThrows(IllegalArgumentException.class, () -> continet.addTerritory(territory2, "id3"));
+
+        Territory territory3 = new Territory("Territory3");
+
+        assertThrows(IllegalArgumentException.class, () -> continet.addTerritory(territory3, "id1"));
+
+        continet.removeTerritory("id1");
+
+        assertEquals(1, continet.getTerritories().values().size());
+        continet.addTerritory(territory3, "id1");
+        assertEquals(2, continet.getTerritories().values().size());
+        assertEquals(territory3, continet.getTerritories().get("id1"));
+
+    }
+
+    // test : testEqualContinent
+    @Test
+    public void testEqualContinent() {
+        Continet continet1 = new Continet("Continet1", 1);
+        Continet continet2 = new Continet("Continet1", 1);
+        Continet continet3 = new Continet("Continet2", 1);
+        Continet continet4 = new Continet("Continet1", 2);
+
+        assertEquals(continet1, continet2);
+        assertEquals(continet1.hashCode(), continet2.hashCode());
+
+        assertNotEquals(continet1, continet3);
+        assertNotEquals(continet1, continet4);
+        assertNotEquals(continet3, continet4);
+
+        assertEquals(continet2, continet1);
+    }
+
+    // test : TestOwedTerritory
+    @Test
+    public void TestOwedTerritory() {
+        Continet continet1 = new Continet("Continet1", 1);
+
+        PlayerPlaceholder player1 = new PlayerPlaceholder("player1");
+        PlayerPlaceholder player2 = new PlayerPlaceholder("player2");
+
+        Territory territory1 = new Territory("Territory1");
+        Territory territory2 = new Territory("Territory2");
+        Territory territory3 = new Territory("Territory3");
+        Territory territory4 = new Territory("Territory4");
+
+        continet1.addTerritory(territory1, "id1");
+        continet1.addTerritory(territory2, "id2");
+        continet1.addTerritory(territory3, "id3");
+        continet1.addTerritory(territory4, "id4");
+
+        territory1.setOwner(player1);
+        territory2.setOwner(player1);
+        territory3.setOwner(player1);
+        territory4.setOwner(player2);
+
+        assertEquals(3, continet1.getOwnedTerritories(player1).size());
+        assertEquals(1, continet1.getOwnedTerritories(player2).size());
+
+    }
+}
