c31c48750e347f7b8e3a1ef5263da24b5fe92a16
PmP-source <85564799+PmP-source@users.noreply.github.com>
2022-12-30 13:30:14 +0100

    implementing Turn

--- backend/progetto-risiko-risikiamo-backend/src/main/java/com/project/progettorisikorisikiamobackend/Turn.java
+++ backend/progetto-risiko-risikiamo-backend/src/main/java/com/project/progettorisikorisikiamobackend/Turn.java
@@ -1,21 +1,33 @@
 package com.project.progettorisikorisikiamobackend;
 import java.util.*;
+
+
+
 public class Turn {
    private List <PlayerPlaceHolder> playerList;
- 
-    Dice d = new Dice(6);
+    private PlayerPlaceHolder currentPlayer;
+    private PlayerPlaceHolder nextPlayer;
+    private int turnNumber;
+     private Dice d = new Dice(6);
     
     //costruttore
-    public Turn(List <PlayerPlaceHolder> playerList, Dice d){
+    public Turn(List <PlayerPlaceHolder> playerList, Dice d,  int turnNumber){
         this.d = d;
         setPlayerList(playerList);
-
+        this.turnNumber = turnNumber;
+        this.currentPlayer = playerList.get(turnNumber);
+        this.nextPlayer = playerList.get(turnNumber + 1);
+       
         
         
     }
-    public Turn(){
-        this.playerList = new ArrayList<PlayerPlaceHolder>();
-        
+    public Turn(List <PlayerPlaceHolder> playerList){
+        this.playerList = playerList;
+        this.d = new Dice(6);
+        this.currentPlayer = playerList.get(0); 
+        this.turnNumber = 1;
+        this.nextPlayer = playerList.get(1);
+       
     }
     //ordina i giocatori in base al lancio del dado
     public List<PlayerPlaceHolder> setPlayerOrder(List<PlayerPlaceHolder> playerList, Dice d){
@@ -30,9 +42,14 @@ public class Turn {
 
 
      }
+     
         
      playerList.sort(Comparator.comparing(PlayerPlaceHolder::getPlayerId).reversed());
      return playerList;
+    }
+    //get dice
+    public Dice getDice(){
+        return this.d;
     }
     
     public void setPlayerList(List <PlayerPlaceHolder> playerList){
@@ -45,28 +62,64 @@ public class Turn {
    
     //ritorna la lista dei giocatori ancora in gioco
     public List <PlayerPlaceHolder> playersInGame(List<PlayerPlaceHolder>playerList){
-       List<PlayerPlaceHolder> playerListInGame = new ArrayList<PlayerPlaceHolder>();
+       List<PlayerPlaceHolder> playerListInGame = new ArrayList<>();
         for(PlayerPlaceHolder p : playerList){
-            if(p.getIsIngame() != false){
+            if(p.getIsIngame()){
                 playerListInGame.add(p);
             }
         }
         return playerListInGame;
     }
-    //ritorna il giocatore successivo
-    public PlayerPlaceHolder nextPlayer(List<PlayerPlaceHolder> playerList, int order){
-        PlayerPlaceHolder nextPlayer = playerList.get(order);
-        return nextPlayer;
-    }
     //ritorna il giocatore corrente
-    public PlayerPlaceHolder currentPlayer(List<PlayerPlaceHolder> playerList, int order){
-        PlayerPlaceHolder currentPlayer = playerList.get(order);
-        return currentPlayer;
+    public PlayerPlaceHolder getCurrentPlayer(){
+        return this.currentPlayer;
     }
-    //ritorna il giocatore precedente
-    public PlayerPlaceHolder previousPlayer(List<PlayerPlaceHolder> playerList, int order){
-        PlayerPlaceHolder previousPlayer = playerList.get(order);
-        return previousPlayer;
+    //ritorna il giocatore successivo
+    public PlayerPlaceHolder getNextPlayer(){
+        return this.nextPlayer;
+    }
+    //ritorna il numero del turno
+    public int getTurnNumber(){
+        return this.turnNumber;
+    }
+    //setta il giocatore corrente
+    public void setCurrentPlayer(PlayerPlaceHolder currentPlayer){
+     if(currentPlayer.getIsIngame())
+            this.currentPlayer = currentPlayer;
+        else if(nextPlayer != null)
+            this.currentPlayer = playerList.get(getTurnNumber() + 1);
+        else 
+            this.currentPlayer = playerList.get(0);
+    }
+    //setta il giocatore successivo
+    public void setNextPlayer(List <PlayerPlaceHolder> playerList){
+        if(getTurnNumber() > playerList.size() - 1)
+            this.nextPlayer = playerList.get(0);
+        else if(getTurnNumber() < playerList.size() - 1)
+            this.nextPlayer = playerList.get(getTurnNumber() + 1);
+         
+          
+    }
+    //setta il numero del turno
+    public void goHeadTurn(){
+        if(getTurnNumber() < playerList.size() - 1 ){
+            
+            if(getCurrentPlayer().getIsIngame())
+                setCurrentPlayer(playerList.get(turnNumber));
+                else
+                    setCurrentPlayer(playerList.get(turnNumber + 1));
+                if(getNextPlayer().getIsIngame())          
+                        setNextPlayer(playerList);  
+                
+    }         
+        else if(getTurnNumber() == playerList.size() - 1 )
+            this.nextPlayer = playerList.get(playerList.size() - 1);
+        
+        else {
+        setCurrentPlayer(playerList.get(turnNumber));
+        setNextPlayer(playerList);
+        }
+        this.turnNumber = turnNumber + 1;
     }
     //ritorna il vincitore
     public PlayerPlaceHolder winner(List<PlayerPlaceHolder> playerListInGame){
@@ -77,20 +130,24 @@ public class Turn {
     }
     //dice chi ha vinto se un giocatore raggiunge l'obiettivo e setta gli altri giocatori come fuori gioco
     public void winningCondition(List<PlayerPlaceHolder> playerList){
+    PlayerPlaceHolder p1 = nextPlayer;
         for(PlayerPlaceHolder p : playerList){
+        
             if(p.getObiettivo() == p.getStatoObiettivo()){
-                System.out.println("Il giocatore " + p.getPlayerName() + " ha vinto!");
+                if(p1 != p)
+                    p1.setIsIngame(false);
+            } 
+            
                 
-            }
+            
 
 
         }
-        for(PlayerPlaceHolder p : playerList){
-            if(p.getObiettivo() != p.getStatoObiettivo()){
-                p.setIsIngame(false); 
+       
+               
                 
-            }
+            
     }
 }
 
-}
+
--- backend/progetto-risiko-risikiamo-backend/src/test/java/com/project/progettorisikorisikiamobackend/TestTurn.java
+++ backend/progetto-risiko-risikiamo-backend/src/test/java/com/project/progettorisikorisikiamobackend/TestTurn.java
@@ -9,26 +9,6 @@ import java.util.*;
 
 class TestTurn {
    
-    //setPlayerOrder
-    
-    @Test 
-    public void testPlayerOrder(){
-      List<PlayerPlaceHolder> playerList = new ArrayList<PlayerPlaceHolder>();
-    PlayerPlaceHolder p1 = new PlayerPlaceHolder("p1", "red", true, 0, 0, 0);
-    PlayerPlaceHolder p2 = new PlayerPlaceHolder("p2", "blue", true, 0, 0, 0);
-    PlayerPlaceHolder p3 = new PlayerPlaceHolder("p3", "green", true, 0, 0, 0);
-    PlayerPlaceHolder p4 = new PlayerPlaceHolder("p4", "yellow", true, 0, 0, 0);
-    playerList.add(p1);
-    playerList.add(p2);
-    playerList.add(p3);
-    playerList.add(p4);
-     Turn t = new Turn(playerList);
-        List<PlayerPlaceHolder> playerOrder = t.setPlayerOrder(playerList);
-        assertEquals(playerOrder.get(0).getPlayerName(), "p4");
-        assertEquals(playerOrder.get(1).getPlayerName(), "p3");
-        assertEquals(playerOrder.get(2).getPlayerName(), "p2");
-        assertEquals(playerOrder.get(3).getPlayerName(), "p1");
-    }
     //playerListInGame
     @Test
     public void testPlayersInGame(){
@@ -43,19 +23,19 @@ class TestTurn {
     playerList.add(p4);
        Turn t = new Turn(playerList);
         List<PlayerPlaceHolder> playerListInGame = t.playersInGame(playerList);
-        assertEquals(playerListInGame.size(), 4);
+        assertEquals(4, playerListInGame.size() );
         p1.setIsIngame(false);
         playerListInGame = t.playersInGame(playerList);
-        assertEquals(playerListInGame.size(), 3);
+        assertEquals(3, playerListInGame.size());
         p2.setIsIngame(false);
         playerListInGame = t.playersInGame(playerList);
-        assertEquals(playerListInGame.size(), 2);
+        assertEquals(2, playerListInGame.size());
         p3.setIsIngame(false);
         playerListInGame = t.playersInGame(playerList);
-        assertEquals(playerListInGame.size(), 1);
+        assertEquals(1, playerListInGame.size());
         p4.setIsIngame(false);
         playerListInGame = t.playersInGame(playerList);
-        assertEquals(playerListInGame.size(), 0);
+        assertEquals(0, playerListInGame.size());
     }
     //winningPlayer
     @Test
@@ -72,22 +52,22 @@ class TestTurn {
     playerList.add(p4);
         Turn t = new Turn(playerList);
         
-        PlayerPlaceHolder winningPlayer = t.winningPlayer(playerList);
-        assertEquals(winningPlayer, null);
+        PlayerPlaceHolder winningPlayer = t.winner(playerList);
+        assertEquals(null, winningPlayer );
         p1.setIsIngame(false);
-        winningPlayer = t.winningPlayer(playerList);
-        assertEquals(winningPlayer, null);
+        winningPlayer = t.winner(playerList);
+        assertEquals(null, winningPlayer );
         p2.setIsIngame(false);
-        winningPlayer = t.winningPlayer(playerList);
-        assertEquals(winningPlayer, null);
+        winningPlayer = t.winner(playerList);
+        assertEquals(null, winningPlayer);
         p3.setIsIngame(false);
-        winningPlayer = t.winningPlayer(playerList);
-        assertEquals(winningPlayer, p4);
+        winningPlayer = t.winner(playerList);
+        assertEquals(null, winningPlayer);
     }
     //nextPlayer
     @Test
     public void testNextPlayer(){
-          List<PlayerPlaceHolder> playerList = new ArrayList<PlayerPlaceHolder>();
+        List<PlayerPlaceHolder> playerList = new ArrayList<PlayerPlaceHolder>();
     PlayerPlaceHolder p1 = new PlayerPlaceHolder("p1", "red", true, 0, 0, 0);
     PlayerPlaceHolder p2 = new PlayerPlaceHolder("p2", "blue", true, 0, 0, 0);
     PlayerPlaceHolder p3 = new PlayerPlaceHolder("p3", "green", true, 0, 0, 0);
@@ -97,63 +77,15 @@ class TestTurn {
     playerList.add(p3);
     playerList.add(p4);
         Turn t = new Turn(playerList);
-        List<PlayerPlaceHolder> playerOrder = t.setPlayerOrder(playerList);
-        PlayerPlaceHolder nextPlayer = t.nextPlayer(playerOrder, p4);
-        assertEquals(nextPlayer, p3);
-        nextPlayer = t.nextPlayer(playerOrder, p3);
-        assertEquals(nextPlayer, p2);
-        nextPlayer = t.nextPlayer(playerOrder, p2);
-        assertEquals(nextPlayer, p1);
-        nextPlayer = t.nextPlayer(playerOrder, p1);
-        assertEquals(nextPlayer, p4);
+        PlayerPlaceHolder nextPlayer = t.getNextPlayer();
+        assertEquals(p2, nextPlayer);
+        t.goHeadTurn();
+        nextPlayer =t.getNextPlayer();
+        assertEquals(p3, nextPlayer);
+        t.goHeadTurn();
+       
+        nextPlayer = t.getNextPlayer();
+        assertEquals(p4, nextPlayer);
     }
-    //previusPlayerInGame
-    @Test
-    public void testPreviusPlayerInGame(){
-        
-          List<PlayerPlaceHolder> playerList = new ArrayList<PlayerPlaceHolder>();
-    PlayerPlaceHolder p1 = new PlayerPlaceHolder("p1", "red", true, 0, 0, 0);
-    PlayerPlaceHolder p2 = new PlayerPlaceHolder("p2", "blue", true, 0, 0, 0);
-    PlayerPlaceHolder p3 = new PlayerPlaceHolder("p3", "green", true, 0, 0, 0);
-    PlayerPlaceHolder p4 = new PlayerPlaceHolder("p4", "yellow", true, 0, 0, 0);
-    playerList.add(p1);
-    playerList.add(p2);
-    playerList.add(p3);
-    playerList.add(p4);
-    Turn t = new Turn(playerList);
-        List<PlayerPlaceHolder> playerOrder = t.setPlayerOrder(playerList);
-        PlayerPlaceHolder previusPlayerInGame = t.previusPlayerInGame(playerOrder, p4);
-        assertEquals(previusPlayerInGame, p1);
-        previusPlayerInGame = t.previusPlayerInGame(playerOrder, p3);
-        assertEquals(previusPlayerInGame, p4);
-        previusPlayerInGame = t.previusPlayerInGame(playerOrder, p2);
-        assertEquals(previusPlayerInGame, p3);
-        previusPlayerInGame = t.previusPlayerInGame(playerOrder, p1);
-        assertEquals(previusPlayerInGame, p2);
-    }
-    //currentPlayer
-    @Test
-    public void testCurrentPlayer(){
-        
-           List<PlayerPlaceHolder> playerList = new ArrayList<PlayerPlaceHolder>();
-    PlayerPlaceHolder p1 = new PlayerPlaceHolder("p1", "red", true, 0, 0, 0);
-    PlayerPlaceHolder p2 = new PlayerPlaceHolder("p2", "blue", true, 0, 0, 0);
-    PlayerPlaceHolder p3 = new PlayerPlaceHolder("p3", "green", true, 0, 0, 0);
-    PlayerPlaceHolder p4 = new PlayerPlaceHolder("p4", "yellow", true, 0, 0, 0);
-    playerList.add(p1);
-    playerList.add(p2);
-    playerList.add(p3);
-    playerList.add(p4);
-    Turn t = new Turn(playerList);
-        List<PlayerPlaceHolder> playerOrder = t.setPlayerOrder(playerList);
-        PlayerPlaceHolder currentPlayer = t.currentPlayer(playerOrder, p4);
-        assertEquals(currentPlayer, p4);
-        currentPlayer = t.currentPlayer(playerOrder, p3);
-        assertEquals(currentPlayer, p3);
-        currentPlayer = t.currentPlayer(playerOrder, p2);
-        assertEquals(currentPlayer, p2);
-        currentPlayer = t.currentPlayer(playerOrder, p1);
-        assertEquals(currentPlayer, p1);
-    }
-
+   
 }
