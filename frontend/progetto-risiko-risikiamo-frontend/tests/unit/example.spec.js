6daf728cfef514d1fe275c884a69361f13c7dcc2
fraking00 <75484827+fraking00@users.noreply.github.com>
2023-01-02 13:30:36 +0100

    test bugged but code working

--- frontend/progetto-risiko-risikiamo-frontend/tests/unit/example.spec.js
+++ frontend/progetto-risiko-risikiamo-frontend/tests/unit/example.spec.js
@@ -1,12 +1,24 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+import { mount } from '@vue/test-utils'
+import HomeComponent from '../../src/components/HomeComponent.vue'
+
+describe('select italy', () => {
+
+  const wrapper = mount(HomeComponent)
+  console.log(wrapper)
+
+  test('does a wrapper exist', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  test('does change over value function', () => {
+    wrapper.vm.changeHoverValue('{"mouseEvent": {"target": "Italy"}}')
+    expect(wrapper.vm.hoverValue).toBe('Italy')
+  })
+
+
+  test('does change over value function 2', () => {
+    wrapper.vm.changeHoverValue('France')
+    expect(wrapper.vm.hoverValue).toBe('place your mouse over a country')
   })
 })
