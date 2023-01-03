659dc94b1b024356d6b4a1c77a034f082317cdff
fraking00 <75484827+fraking00@users.noreply.github.com>
2023-01-03 15:48:59 +0100

    test modified and all passed

--- frontend/progetto-risiko-risikiamo-frontend/src/routers.js
+++ frontend/progetto-risiko-risikiamo-frontend/src/routers.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeVue from './components/HomeComponent.vue'
+import NestedRoute from "./components/NestedRoute.vue"
 
 const routes = [
     {
         name: "home",
         component: HomeVue,
         path: "/"
+    },
+    {
+        name: "nested-route",
+        component: NestedRoute,
+        path: "/nested-route"
     }
 ];
 
