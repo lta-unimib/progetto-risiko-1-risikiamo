62e459bb038286ebb0e1e82da04fa68d32c2087a
PmP-source <85564799+PmP-source@users.noreply.github.com>
2023-01-11 18:21:30 +0100

    initial commit

---
+++ backend/progetto-risiko-risikiamo-backend/src/main/java/com/project/progettorisikorisikiamobackend/Cards/Deck.java
@@ -0,0 +1,13 @@
+package com.project.progettorisikorisikiamobackend.Cards;
+
+import java.util.Map;
+import lombok.*;
+public  abstract class Deck {
+    private @Getter Map<?, String> cards;
+    Deck(Map< ? , String> cards){
+        this.cards = cards;
+    }
+    public abstract void description(Object obj);
+    public abstract Object draw();
+   
+}
