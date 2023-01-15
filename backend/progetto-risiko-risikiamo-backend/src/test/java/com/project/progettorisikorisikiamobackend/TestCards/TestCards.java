dc226061349efcec7dc16128958bc6a2e622b0c6
PmP-source <85564799+PmP-source@users.noreply.github.com>
2023-01-15 17:46:38 +0100

    fixTest

--- backend/progetto-risiko-risikiamo-backend/src/test/java/com/project/progettorisikorisikiamobackend/TestCards/TestCards.java
+++ backend/progetto-risiko-risikiamo-backend/src/test/java/com/project/progettorisikorisikiamobackend/TestCards/TestCards.java
@@ -13,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import java.util.ArrayList;
 import com.project.progettorisikorisikiamobackend.Cards.*;
+import com.project.progettorisikorisikiamobackend.obiettivi.Objective;
 
 
 
@@ -41,8 +42,8 @@ void testDeckObjectives() {
     assertEquals(OpponentDefeated.class, deck.getDeck().get(0).getClass() );
     assertEquals(OpponentDefeated.class, deck.getDeck().get(1).getClass());
     
-    
-    assertTrue(OpponentDefeated.class == deck.draw(player1).getClass() || TotTerritories.class == deck.draw(player1).getClass()|| ConquestContinent.class == deck.draw(player1).getClass());
+    Objective cartaPescata = deck.draw(player1);
+    assertTrue(OpponentDefeated.class == cartaPescata.getClass() || TotTerritories.class == cartaPescata.getClass()|| ConquestContinent.class ==  cartaPescata.getClass());
     
        
 
@@ -84,8 +85,10 @@ void testReedemCards() {
     Continent c1 = new Continent("c1", 1);
     Territory t1 = new Territory("t1");
     Territory t2 = new Territory("t2");
+    Territory t22 = new Territory("t22");
     c1.addTerritory(t2);
     c1.addTerritory(t1);
+    c1.addTerritory(t22);
     map.addContinent(c1, "c1");
     List<Player> players = new ArrayList<>();
     Player player1 = new Player("player1");
@@ -94,19 +97,20 @@ void testReedemCards() {
     players.add(player2);
     t1.setOwner(player2);
     t2.setOwner(player2);
-    try{
+    t22.setOwner(player2);
+  
     DeckTerritories daReedem = new DeckTerritories( map, players );
-    CardTerritory card1 = new CardTerritory("c1", EnumCard.CANNONE);
-    CardTerritory card2 = new CardTerritory("c2", EnumCard.CANNONE);
-    CardTerritory card3 = new CardTerritory("c3", EnumCard.CANNONE);
+    CardTerritory card1 = new CardTerritory("t1", EnumCard.CANNONE);
+    CardTerritory card2 = new CardTerritory("t2", EnumCard.CANNONE);
+    CardTerritory card3 = new CardTerritory("t22", EnumCard.CANNONE);
    
     List <CardTerritory> cards = new ArrayList<>();
     cards.add(card1);
     cards.add(card2);
     cards.add(card3);
-    daReedem.setDeck(cards);
-    assertEquals(4, daReedem.reedemCards(card1, card2, card3, player2));
-    assertEquals(0, daReedem.getDeck().size());
+
+    assertEquals(4, daReedem.reedemCards(card1, card2, card3, player1));
+    assertEquals(6, daReedem.getDeck().size());
     Territory t3 = new Territory("t3");
     Territory t4 = new Territory("t4");
     Territory t5 = new Territory("t5");
@@ -126,9 +130,10 @@ void testReedemCards() {
     cards2.add(card5);
     cards2.add(card6);
    
-   daReedem.setDeck(cards2);
+   
     assertEquals(10, daReedem.reedemCards(card4, card5, card6, player2));
-    assertEquals(3, daReedem.getDeck().size());
+    assertEquals(9, daReedem.getDeck().size());
+    daReedem.setDeck(cards2);
     assertEquals(card4, daReedem.getDeck().get(0));
     assertEquals(card5, daReedem.getDeck().get(1));
     assertEquals(card6, daReedem.getDeck().get(2));
@@ -143,11 +148,8 @@ void testReedemCards() {
     assertEquals(0, daReedem.reedemCards(card7, card8, card9, player2));
     
 
-    }catch(Exception e) {
-        System.out.println(e.getMessage());
-    }finally {
-        System.out.println("test");
-    }
+  
+    
     }
    
 @Test
