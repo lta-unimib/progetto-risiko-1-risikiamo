c6d97f4fa4da2bb70e72bd1d29aea61ac91a92a8
MauroZorzin <m.zorzin@campus.unimib.it>
2023-01-21 13:44:45 +0100

    .

--- backend/progetto-risiko-risikiamo-backend/src/main/java/com/project/progettorisikorisikiamobackend/services/responce/GameDto.java
+++ backend/progetto-risiko-risikiamo-backend/src/main/java/com/project/progettorisikorisikiamobackend/services/responce/GameDto.java
@@ -2,8 +2,6 @@ package com.project.progettorisikorisikiamobackend.services.responce;
 
 import java.util.List;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import lombok.Data;
 
 @Data
@@ -14,7 +12,7 @@ public class GameDto {
     private String id;
     private String winner;
     private MapDto map;
-    private List<Pair<String, String>> players;
+    private List<InternalPlayerDto> players;
     private String currentPlayer;
 
 }
